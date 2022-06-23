49cf8200386de554e9abbd3220cfed3892f67c15
Ramen-Nood1ez <rommelc000@gmail.com>
2022-06-22 22:23:06 -0500

    #25

--- admin/panel.php
+++ admin/panel.php
@@ -11,8 +11,8 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">  
-	<link rel="stylesheet" href="public/style.css">
+	<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">  
+	<link rel="stylesheet" href="/public/style.css">
 	<title>Hogosha Judo Admin Panel</title>
 </head>
 <body>
