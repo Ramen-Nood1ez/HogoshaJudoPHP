9a8467ee0844d8e142cb33323c8d76c55bd11725
Ramen-Nood1ez <rommelc000@gmail.com>
2022-06-22 22:21:50 -0500

    #23

--- admin/panel.php
+++ admin/panel.php
@@ -11,9 +11,18 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">  
+	<link rel="stylesheet" href="public/style.css">
 	<title>Hogosha Judo Admin Panel</title>
 </head>
 <body>
+	<div id="includedContent" class="topnav">
+		<?php 
+			$myfile = fopen("navbar.php", "r") or die("Unable to load navbar!");
+			echo fread($myfile, filesize("navbar.php"));
+			fclose($myfile);
+		?>
+	</div>
 	
 </body>
 </html>
