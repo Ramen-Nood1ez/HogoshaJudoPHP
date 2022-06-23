d40045ea7628d6c189322237c0220270e5f87d9e
Ramen-Nood1ez <rommelc000@gmail.com>
2022-06-22 22:56:06 -0500

    #31

--- admin/uploader.php
+++ admin/uploader.php
@@ -9,8 +9,10 @@
 <?php 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$morephotos = $_POST["morephotos"];
-
-		$target_dir = "/morephotos/";
+		chdir("..");
+		chdir("morephotos");
+		$d = dir(getcwd());
+		$target_dir = $d;
 		$target_file = $target_dir . basename($_FILES["file"]["name"]);
 		$uploadOk = 1;
 		$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
