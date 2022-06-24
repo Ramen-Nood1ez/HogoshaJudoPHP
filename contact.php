f13b8b4ff8a8ec5b029f90291c22b498a249a1a9
Ramen-Nood1ez <rommelc000@gmail.com>
2022-06-23 22:18:21 -0500

    #45

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
 		<!--link rel="stylesheet" href="style.css"-->
-		<link rel="stylesheet" href="public/newstyle.css">
+		<link rel="stylesheet" href="/public/newstyle.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://kit.fontawesome.com/8e00593c4c.js" crossorigin="anonymous"></script>
 		<title>Hogosha Judo</title>
