a6efb4da72f4b911b97f4c5fcab48f62e008f4ef
Ramen-Nood1ez <rommelc000@gmail.com>
2022-06-23 22:43:31 -0500

    #51

--- admin/panel.php
+++ admin/panel.php
@@ -30,7 +30,7 @@
 
 	<section class="panel-grid-layout">
 		<h2 style="grid-area: headone;">File Utils</h2>
-		<div class="panel-grid-row">
+		<div style="grid-area: row1;" class="panel-grid-row">
 			<a href="upload.php">Upload an image/video</a>
 			<a href="#">Unused</a>
 			<a href="#">Unused</a>
@@ -38,7 +38,7 @@
 			<a href="#">Unused</a>
 		</div>
 		<h2 style="grid-area: headtwo;">Useful Links</h2>
-		<div class="panel-grid-row">
+		<div style="grid-area: row1;" class="panel-grid-row">
 			<a href="https://cpanel.hogoshajudo.org/">Cpanel</a>
 			<a href="https://webmail.hogoshajudo.org/">Webmail</a>
 			<a href="#">Unused</a>
