5d24cd8363f349fa7216947d9f70072926963b6e
Ramen-Nood1ez <rommelc000@gmail.com>
2022-06-23 21:59:37 -0500

    #39

--- login/login.php
+++ login/login.php
@@ -104,7 +104,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Hogosha Judo File Share</title>
-	<link rel="stylesheet" href="/public/style.css">
+	<!--link rel="stylesheet" href="/public/style.css"-->
+	<link rel="stylesheet" href="~/newstyle.css">
 </head>
 <body>
 	<h2>Login</h2>
