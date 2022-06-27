be77ef32819ae99ac4cc42464a9f57bdabe92ad7
Ramen-Nood1ez <rommelc000@gmail.com>
2022-06-26 21:53:01 -0500

    #82

--- photos.php
+++ photos.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-		<link rel="stylesheet" href="public/style.css">
+		<link rel="stylesheet" href="public/newstyle.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://kit.fontawesome.com/8e00593c4c.js" crossorigin="anonymous"></script>
 		<script> 
