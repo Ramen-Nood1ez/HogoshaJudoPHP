bf5b0245e28a4babcb6b6c53e1236b5853b0e460
Ramen-Nood1ez <rommelc000@gmail.com>
2022-06-26 19:46:58 -0500

    #59

--- navbar.php
+++ navbar.php
@@ -9,6 +9,22 @@
 	<a href="/morephotos.php">More Photos</a>
 	<a href="/contact.php">Contact Us</a>
 	<!--a href="/subscribe.php">Subscribe</a-->
+	<?php 
+	// Check if user is logged in, and show them things they have permissions to see
+	session_start();
+
+	if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+		$permlevel = $_SESSION["permlevel"];
+
+		if ($permlevel >= 3) {
+			echo "<a href='/admin/panel.php'>Admin Panel</a>";
+		}
+
+		echo "<a href='/login/logout.php'>Logout</a>";
+	} else {
+		echo "<a href='/login/login.php'>Login</a>";
+	}
+	?>
 	<a href="" onclick="ToggleDarkMode()">Toggle Dark Mode</a>
 	<a href="#" onclick="toggleTopNav()" class="icon">☰</a>
 	<script>
