7c5a9608c74a49d15fd2a7d0ce0e03ed86ba5856
Ramen-Nood1ez <rommelc000@gmail.com>
2022-07-17 18:28:41 -0500

    #119

--- visitor.php
+++ visitor.php
@@ -1,5 +1,11 @@
 <?php 
 	session_start();
-	echo $_SERVER['REMOTE_ADDR'];
-	echo inet_pton($_SERVER['REMOTE_ADDR']);
+	echo $_SERVER['REMOTE_ADDR'] . "\n";
+	$packed = inet_pton($_SERVER['REMOTE_ADDR']);
+	if ($packed == false) {
+		echo "IP address is not valid\n";
+	}
+	else {
+		echo "$packed";
+	}
 ?>
