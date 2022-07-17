77c0d9027b5de84e0b01f1b62a44903dd551da35
Ramen-Nood1ez <rommelc000@gmail.com>
2022-07-17 14:12:52 -0500

    #90

--- announcement.php
+++ announcement.php
@@ -40,7 +40,7 @@
 
 	echo $end_date;
 
-	$sql = "SELECT id, title, description, type, end_date FROM announcements WHERE id=(SELECT MAX(id) FROM announcements)";
+	$sql = "SELECT id, title, description, type, end_date FROM announcements WHERE id=1";
 
 	if ($stmt = mysqli_prepare($link, $sql)) {
 		// Attempt to execute prepared statement
