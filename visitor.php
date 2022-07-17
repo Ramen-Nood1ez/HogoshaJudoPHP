d6f45217b952887a7b9cd68eeccc426941656fa6
Ramen-Nood1ez <rommelc000@gmail.com>
2022-07-17 18:48:43 -0500

    #123

--- visitor.php
+++ visitor.php
@@ -9,21 +9,26 @@
 	$selected_addr = "";
 
 	if ($stmt = mysqli_prepare($link, $sql)) {
+		echo "Prepare\n";
 		// Attempt to execute prepared statement
 		if (mysqli_stmt_execute($stmt)) {
+			echo "execute\n";
 			// Store result
 			mysqli_stmt_store_result($stmt);
+			echo "store\n";
 
 			// Bind result variables
 			mysqli_stmt_bind_result($stmt, $selected_addr);
+			echo "bind\n";
 			
 			if (mysqli_stmt_fetch($stmt)) {
+				echo "fetch\n";
 				echo "Found ip:\t\t$selected_addr\n";
 			} else {
-				echo "Fetch went wrong!";
+				echo "Fetch went wrong!\n";
 			}
 		} else {
-			echo "Oops! Something went wrong. Please try again later.";
+			echo "Oops! Something went wrong. Please try again later.\n";
 		}
 
 		// Close statement
