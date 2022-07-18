c3b6f3125aab082e164c3b076813493bc06c8717
Ramen-Nood1ez <rommelc000@gmail.com>
2022-07-17 21:37:05 -0500

    #160

--- visitor.php
+++ visitor.php
@@ -4,7 +4,7 @@
 	if ($_COOKIE["hasvisited"] == true) {
 		exit;
 	} else {
-		setcookie("hasvisited", false, time() + 50000, "/");
+		setcookie("hasvisited", false, 0, "/");
 	}
 	// echo $_SERVER['REMOTE_ADDR'] . "\n";
 
