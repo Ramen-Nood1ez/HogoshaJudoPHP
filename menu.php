df9960d714d5f430aa4252837596db21c5ee4b4f
Ramen-Nood1ez <rommelc000@gmail.com>
2022-07-19 13:41:42 -0500

    #167

---
+++ menu.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">  
+		<link rel="stylesheet" href="/public/newstyle.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://kit.fontawesome.com/8e00593c4c.js" crossorigin="anonymous"></script>
+		<title>Hogosha Judo</title>
+	</head>
+	<body style="text-align: center;">
+		<div class="grid-container">
+			<div id="includedContent" class="topnav">
+				<?php 
+					include("navbar.php");
+				?>
+			</div>
+
+			<main>
+				<img class="logo" width="300" height="320" alt="Hogosha Judo" src="images/Registered-Hogosha-Logo-3.jpg">
+				<div class="menu">
+					<p>Test 1</p> <p>Price 1</p>
+					<p>Test 2</p> <p>Price 2</p>
+				</div>
+			</main>
+	</body>
+</html>
