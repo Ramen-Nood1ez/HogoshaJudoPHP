98fad27615be83115f945df175ddb00e2824c9e3
Ramen-Nood1ez <rommelc000@gmail.com>
2022-07-19 22:32:09 -0500

    #224

--- announcement.php
+++ announcement.php
@@ -145,7 +145,8 @@
 			<main>
 				<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 					<label for="creator_id">Creator ID</label>
-					<input type="number" name="creator_id">
+					<input type="number" name="creator_id" 
+						value="<?php session_start(); echo $_SESSION['id']; ?>">
 					<br>
 					<label for="title">Title</label>
 					<input type="text" name="title">
